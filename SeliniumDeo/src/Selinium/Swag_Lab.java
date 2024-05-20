8e04df36f9110875d68fdc6877327d0e67bc329b
vaishnavi20014 <167406630+vaishnavi20014@users.noreply.github.com>
2024-05-20 10:34:23 +0530

    2nd commit

---
+++ SeliniumDeo/src/Selinium/Swag_Lab.java
@@ -0,0 +1,10 @@
+package Selinium;
+
+public class Swag_Lab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- SeliniumDeo/src/Selinium/Swaglabs.java
+++ SeliniumDeo/src/Selinium/Swaglabs.java
@@ -32,11 +32,55 @@ public class Swaglabs {
 		String vaish=driver.getWindowHandle();
 		driver.switchTo().window(vaish);
 		
+		
+		
 		driver.findElement(By.linkText("Sauce Labs Backpack")).click();
 		Thread.sleep(1000);
 		
-		String currentwindowHandle1 =driver.getWindowHandle();
-		driver.findElement(By.xpath())
+		 String currentwindowHandle1 = driver.getWindowHandle();
+	     driver.switchTo().window(currentwindowHandle1);
+	     
+		driver.findElement(By.xpath("//button[text()='ADD TO CART']")).click();
+		Thread.sleep(1000);
+		
+		/* System.setProperty("webdriver.chrome.driver", "Driver\\chromedriver.exe");
+	       // System.setProperty("webdriver.chrome.driver", "driver\\chromedriver.exe");
+	       WebDriver driver = new ChromeDriver();// open the browser
+
+	       driver.manage().window().maximize();// maximize the windows
+	       driver.get("https://www.saucedemo.com/v1/");
+	       WebElement username = driver.findElement(By.id("user-name"));
+	       WebElement password = driver.findElement(By.id("password"));
+
+	       Thread.sleep(1000);
+	       username.sendKeys("standard_user");
+	       
+	       Thread.sleep(1000);
+	       password.sendKeys("secret_sauce");
+
+	       WebElement Login = driver.findElement(By.id("login-button"));
+	       // id="login-button"
+	       Login.click();
+	       Thread.sleep(1000);
+	       // Switch to Product Page
+	       String currentwindowHandle = driver.getWindowHandle();
+	       driver.switchTo().window(currentwindowHandle);
+
+	       // Sauce Labs Bolt T-Shirt
+	       driver.findElement(By.linkText("Sauce Labs Bolt T-Shirt")).click();
+	       Thread.sleep(1000);
+
+	       // Switch to Product Page
+	       String currentwindowHandle1 = driver.getWindowHandle();
+	       driver.switchTo().window(currentwindowHandle1);
+
+	       // driver.findElement(By.className("btn_primary btn_inventory")).click();
+	       driver.findElement(By.xpath("//button[text()='ADD TO CART']")).click();
+
+	       Thread.sleep(100);*/
+
+	      
+		
 		}
 
 }
