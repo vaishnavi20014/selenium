7c6b796e327ccc74a43bd47271820d807170a755
vaishnavi20014 <167406630+vaishnavi20014@users.noreply.github.com>
2024-05-23 09:48:02 +0530

    commit

--- SeliniumDeo/src/Selinium/demoqa.java
+++ SeliniumDeo/src/Selinium/demoqa.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class demoqa {
 
@@ -19,23 +20,39 @@ public class demoqa {
 		WebElement firstname = driver.findElement(By.id("firstName"));
 		firstname.sendKeys("vaishnavi");
 		Thread.sleep(1000);
+		
 		//lastName
 		WebElement lastName = driver.findElement(By.id("lastName"));
 		lastName.sendKeys("shelke");
 		Thread.sleep(1000);
+		
 		//Email
 		WebElement Email = driver.findElement(By.id("userEmail"));
 		Email.sendKeys("vaishnavishelke39gmail.com");
 		Thread.sleep(1000);
+		
 		//genderbtn
 	    driver.findElement(By.xpath("//input[@id='gender-radio-2']"));
 	    //radiobtn.click();
+	    
 		//userNumber
 		WebElement no = driver.findElement(By.id("userNumber"));
 		no.sendKeys("9850757659");
 		Thread.sleep(1000);
-		////input[@id='dateOfBirthInput']
 		
+		//date of birth
+		WebElement datepicker=driver.findElement(By.className("form-control react-datepicker-ignore-onclickoutside"));
+		WebElement selectDay =driver.findElement(By.xpath("//div[@class='react-datepicker__day react-datepicker__day--025 react-datepicker__day--weekend']"));
+		selectDay.click();
+		
+		//subjects
+		WebElement sub = driver.findElement(By.className("subjects-auto-complete__value-container subjects-auto-complete__value-container--is-multi css-1hwfws3"));
+		sub.sendKeys("Mca");
+		Thread.sleep(1000);
+		
+		//dropdown
+		WebElement state=driver.findElement(By.className("css-19bqh2r"));
+	    Select selectstate = new Select(state);
 		
 		
 		
---
+++ SeliniumDeo/src/Selinium/plagarithm.java
@@ -0,0 +1,10 @@
+package Selinium;
+
+public class plagarithm {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
